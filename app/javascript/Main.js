c1a72300ac432facbf3a6a85c5f3617fc94dc974
Serj Panimatchenko <serjpanima@gmail.com>
2015-02-04 17:05:26 +0200

    some fixes
    replace tabs to spaces;
    replace russian text to english;

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -3,51 +3,51 @@ var tvKey = new Common.API.TVKeyValue();
 
 function AVPlayerInit() {
 
-	var playerInstance = webapis.avplay;
-	playerInstance.getAVPlay(onAVPlayObtained, onGetAVPlayError);
+    var playerInstance = webapis.avplay;
+    playerInstance.getAVPlay(onAVPlayObtained, onGetAVPlayError);
 
-	function onAVPlayObtained(avplay) {
-		Main.AVPlayer = avplay;
-		Main.AVPlayer.init();
-	}
+    function onAVPlayObtained(avplay) {
+        Main.AVPlayer = avplay;
+        Main.AVPlayer.init();
+    }
 
-	function onGetAVPlayError(error) {
-		alert('#####onGetAVPlayError: ' + error.message);
-	}
+    function onGetAVPlayError(error) {
+        // alert('#####onGetAVPlayError: ' + error.message);
+    }
 };
 
 function AVPlayerRun() {
-	Main.AVPlayer.open('http://live.tvhope.cdnvideo.ru/tvhope-pull/tvhope_1/playlist.m3u8|COMPONENT=HLS', {});
-	Main.AVPlayer.play(onSuccessCB, onErrorCB);
+    Main.AVPlayer.open('http://live.tvhope.cdnvideo.ru/tvhope-pull/tvhope_1/playlist.m3u8|COMPONENT=HLS', {});
+    Main.AVPlayer.play(onSuccessCB, onErrorCB);
 
-	function onSuccessCB() {
-		alert('Play Success');
-	}
-	function onErrorCB(error) {
-		alert('Play Error' + error.message);
-	}
+    function onSuccessCB() {
+        // alert('Play Success');
+    }
+    function onErrorCB(error) {
+        // alert('Play Error' + error.message);
+    }
 }
 
 var Main = {};
 Main.onLoad = function() {
-	this.enableKeys();
-	widgetAPI.sendReadyEvent();
+    this.enableKeys();
+    widgetAPI.sendReadyEvent();
 
-	AVPlayerInit();
-	AVPlayerRun();
+    AVPlayerInit();
+    AVPlayerRun();
 };
 
 Main.onUnload = function() {};
 
 Main.enableKeys = function() {
-	document.getElementById('anchor').focus();
+    document.getElementById('anchor').focus();
 };
 
 Main.keyDown = function() {
-	var keyCode = event.keyCode;
+    var keyCode = event.keyCode;
 
-	switch(keyCode) {
-		case tvKey.KEY_RETURN:
-			break;
-	}
+    switch(keyCode) {
+        case tvKey.KEY_RETURN:
+            break;
+    }
 };
